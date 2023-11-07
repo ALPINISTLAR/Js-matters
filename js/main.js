dd4ce2ec88aa8d74995f9c57da7e098faa557e32
ALPINISTLAR <niveam424@gmail.com>
2023-11-07 20:50:44 +0200

    add functions (from 11 to 15)

--- js/main.js
+++ js/main.js
@@ -30,6 +30,9 @@ function daraja() {
     document.getElementById("demo").innerHTML = number ** 3;
 }
 
+function fault(){
+  alert('Bu mavzu hali o`tilmagan!');
+}
 
 function kopaytma() {
   let a = +prompt("Ushbu funksiya sonlar ko`paytmasini hisoblaydi, 1 - sonni kiriting:");
@@ -38,7 +41,7 @@ function kopaytma() {
 }
 
 function soatSekund() {
-  let soat = +prompt("Ushbu funksiya soatlar butun son sifatida qabul qilib, uni sekundlarga o`girib beradi, sonni kiriting:");
+  let soat = +prompt("Ushbu funksiya soatlarni butun son sifatida qabul qilib, uni sekundlarga o`girib beradi, sonni kiriting:");
     document.getElementById("demo").innerHTML = soat * 60 * 60 +' sekund';
 }
 
@@ -46,4 +49,34 @@ function uchburchakTomoni() {
   let tomon1 = +prompt("Ushbu funksiya uchburchakning eng uzun tomonini hisoblaydi, A tomonini kiriting:");
   let tomon2 = +prompt("B tomonini kiriting:");
     document.getElementById("demo").innerHTML = ((tomon1 + tomon2) - 1) + ' ga teng';
+}
+
+function qoldiq() {
+  let son1 = +prompt("Ushbu funksiya ikkita son qabul qiladi va ularning bo`linmasidan qolgan qoldiqni chiqarib beradi, 1 - sonni kiriting:");
+  let son2 = +prompt("2 - sonni kiriting:");
+    document.getElementById("demo").innerHTML = son1 % son2 + ' ga teng';
+}
+
+
+
+
+
+function string() {
+  let string= prompt("Ushbu funksiya “Something” stringiga ” ” bo’sh joy va “a” stringini birlashtirib qaytaradi");
+    document.getElementById("demo").innerHTML = 'Something' + ' ' + string;
+}
+
+function kvadrat() {
+  let raqam = +prompt("Ushbu funksiya soatlarning kvadratini hisoblaydi:");
+    document.getElementById("demo").innerHTML = raqam * raqam;
+}
+
+function taqqoslash() {
+  let raqam = +prompt("Ushbu funksiya raqam qabul qiladi, agar raqam 0 dan kichik yoki teng bo'lsa ROST, aks holda YOLG`ON qaytaradi:");
+  if (raqam <= 0) {
+    document.getElementById("demo").innerHTML = 'ROST';
+  }
+    else {
+      document.getElementById("demo").innerHTML = `YOLG'ON`;
+    }
 }
